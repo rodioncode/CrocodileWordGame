e86fc32d25257f76b6f24490918464b659098680
rodioncode <dubanovrodion@gmail.com>
2022-05-01 16:28:00 +0300

    initial commit

---
+++ app/src/main/java/com/rodiondev/crocodilewordgame/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.rodiondev.crocodilewordgame
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rodiondev.crocodilewordgame.ui.theme.CrocodileWordGameTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CrocodileWordGameTheme {
+                
+            }
+        }
+    }
+}
+@Composable
+fun navigation(): NavController{
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") {  }
+        composable("login") {  }
+        /*...*/
+    }
+    return navController
+}
